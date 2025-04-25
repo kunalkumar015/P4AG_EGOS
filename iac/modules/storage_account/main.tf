aa1ced18d46fd92f578735c4cf7ddd3d316c8232
Mayur Patel <mayurpatel@ups.com>
2025-04-25 14:40:22 -0400

    add sa

--- iac/modules/storage_account/main.tf
+++ iac/modules/storage_account/main.tf
@@ -14,8 +14,8 @@ data "azurerm_resource_group" "resource_group" {
   name = "rg-wwe-${local.environment_sanitized}"
 }
 
-data "azurerm_user_assigned_identity" "mi_user" {
-  name                = "mi-wwe-${local.environment_sanitized}-${local.region_sanitized}"
-  resource_group_name = "rg-wwe-shared-prd-${local.region_sanitized}"
-}
+# data "azurerm_user_assigned_identity" "mi_user" {
+#   name                = "mi-wwe-${local.environment_sanitized}-${local.region_sanitized}"
+#   resource_group_name = "rg-wwe-shared-prd-${local.region_sanitized}"
+# }
 
