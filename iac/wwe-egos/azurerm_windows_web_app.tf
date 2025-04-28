8a4896b4b134752374daae7cb8c9cf2c6958eed0
Mayur Patel <mayurpatel@ups.com>
2025-04-28 11:02:43 -0400

    add more app services for egos

--- iac/wwe-egos/azurerm_windows_web_app.tf
+++ iac/wwe-egos/azurerm_windows_web_app.tf
@@ -9,72 +9,79 @@ module "windows_web_app_dashboard" {
   asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
 }
 
-# module "windows_web_app_webservices" {
-#  source = "../modules/app-services"
-#   environment         = "prd"
-#   organization_suffix = "wwe"
-#   app_name            = "webservices"
-#   app_type            = local.app_type
-#   app_sku             = "P2v2"
-#   region              = local.region_sanitized
-# }
+module "windows_web_app_webservices" {
+ source = "../modules/app-services"
+  environment         = "local.environment_sanitized"
+  organization_suffix = "wwe"
+  app_name            = "webservices"
+  app_type            = local.app_type
+  app_sku             = var.app_sku
+  region              = local.region_sanitized
+  asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
+}
 
-# module "windows_web_app_tradedirect" {
-#  source = "../modules/app-services"
-#   environment         = "prd"
-#   organization_suffix = "wwe"
-#   app_name            = "tradedirect"
-#   app_type            = local.app_type
-#   app_sku             = "P2v2"
-#   region              = local.region_sanitized
-# }
+module "windows_web_app_tradedirect" {
+ source = "../modules/app-services"
+  environment         = "local.environment_sanitized"
+  organization_suffix = "wwe"
+  app_name            = "tradedirect"
+  app_type            = local.app_type
+  app_sku             = var.app_sku
+  region              = local.region_sanitized
+  asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
+}
 
-# module "windows_web_app_opstrackingwebapi" {
-#  source = "../modules/app-services"
-#   environment         = "prd"
-#   organization_suffix = "wwe"
-#   app_name            = "opstrackingwebapi"
-#   app_type            = local.app_type
-#   app_sku             = "P2v2"
-#   region              = local.region_sanitized
-# }
+module "windows_web_app_opstrackingwebapi" {
+ source = "../modules/app-services"
+  environment         = "local.environment_sanitized"
+  organization_suffix = "wwe"
+  app_name            = "opstrackingwebapi"
+  app_type            = local.app_type
+  app_sku             = var.app_sku
+  region              = local.region_sanitized
+  asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
+}
 
-# module "windows_web_app_dao" {
-#  source = "../modules/app-services"
-#   environment         = "prd"
-#   organization_suffix = "wwe"
-#   app_name            = "dao"
-#   app_type            = local.app_type
-#   app_sku             = "P2v2"
-#   region              = local.region_sanitized
-# }
+module "windows_web_app_dao" {
+ source = "../modules/app-services"
+  environment         = "local.environment_sanitized"
+  organization_suffix = "wwe"
+  app_name            = "dao"
+  app_type            = local.app_type
+  app_sku             = var.app_sku
+  region              = local.region_sanitized
+  asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
+}
 
-# module "windows_web_app_egosapi" {
-#  source = "../modules/app-services"
-#   environment         = "prd"
-#   organization_suffix = "wwe"
-#   app_name            = "egosapi"
-#   app_type            = local.app_type
-#   app_sku             = "P2v2"
-#   region              = local.region_sanitized
-# }
+module "windows_web_app_egosapi" {
+ source = "../modules/app-services"
+  environment         = "local.environment_sanitized"
+  organization_suffix = "wwe"
+  app_name            = "egosapi"
+  app_type            = local.app_type
+  app_sku             = var.app_sku
+  region              = local.region_sanitized
+  asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
+}
 
-# module "windows_web_app_hangfire" {
-#  source = "../modules/app-services"
-#   environment         = "prd"
-#   organization_suffix = "wwe"
-#   app_name            = "hangfire"
-#   app_type            = local.app_type
-#   app_sku             = "P2v2"
-#   region              = local.region_sanitized
-# }
+module "windows_web_app_hangfire" {
+ source = "../modules/app-services"
+  environment         = "local.environment_sanitized"
+  organization_suffix = "wwe"
+  app_name            = "hangfire"
+  app_type            = local.app_type
+  app_sku             = var.app_sku
+  region              = local.region_sanitized
+  asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
+}
 
-# module "windows_web_app_nuget" {
-#  source = "../modules/app-services"
-#   environment         = "prd"
-#   organization_suffix = "wwe"
-#   app_name            = "nuget"
-#   app_type            = local.app_type
-#   app_sku             = "P2v2"
-#   region              = local.region_sanitized
-# }
+module "windows_web_app_nuget" {
+ source = "../modules/app-services"
+  environment         = "local.environment_sanitized"
+  organization_suffix = "wwe"
+  app_name            = "nuget"
+  app_type            = local.app_type
+  app_sku             = var.app_sku
+  region              = local.region_sanitized
+  asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
+}
