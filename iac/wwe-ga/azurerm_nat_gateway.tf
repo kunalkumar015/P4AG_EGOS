3c42f2d2d9bc86ac0d40bda8d436f712b61f9228
Mayur Patel <mayurpatel@ups.com>
2025-04-29 14:45:55 -0400

    add nat gw for ga

--- iac/wwe-ga/azurerm_nat_gateway.tf
+++ iac/wwe-ga/azurerm_nat_gateway.tf
@@ -1,22 +1,21 @@
-#delete 1124 recreate 0125
-# resource "azurerm_public_ip_prefix" "natgw" {
-#   name                = "pip-wwe-${local.app_type}-${local.environment_sanitized}-${local.region_sanitized}"
-#   location            = azurerm_resource_group.wwe_ga.location
-#   resource_group_name = azurerm_resource_group.wwe_ga.name
-#   prefix_length       = 29
-#   zones               = ["1"]
-# }
+resource "azurerm_public_ip_prefix" "ga_nat" {
+  name                = "pip-wwe-${local.app_type}-${local.environment_sanitized}-${local.region_sanitized}"
+  location            = data.azurerm_resource_group.resource_group.location
+  resource_group_name = data.azurerm_resource_group.resource_group.name
+  prefix_length       = 29
+  zones               = ["1"]
+}
 
-# resource "azurerm_nat_gateway" "natgw" {
-#   name                    = "nat-wwe-${local.app_type}-${local.environment_sanitized}-${local.region_sanitized}"
-#   location                = azurerm_resource_group.wwe_ga.location
-#   resource_group_name     = azurerm_resource_group.wwe_ga.name
-#   sku_name                = "Standard"
-#   idle_timeout_in_minutes = 10
-#   zones                   = ["1"]
-# }
+resource "azurerm_nat_gateway" "ga_nat" {
+  name                    = "nat-wwe-${local.app_type}-${local.environment_sanitized}-${local.region_sanitized}"
+  location                = data.azurerm_resource_group.resource_group.location
+  resource_group_name     = data.azurerm_resource_group.resource_group.name
+  sku_name                = "Standard"
+  idle_timeout_in_minutes = 10
+  zones                   = ["1"]
+}
 
-# resource "azurerm_nat_gateway_public_ip_prefix_association" "natgw" {
-#   nat_gateway_id      = azurerm_nat_gateway.natgw.id
-#   public_ip_prefix_id = azurerm_public_ip_prefix.natgw.id
-# }
+resource "azurerm_nat_gateway_public_ip_prefix_association" "ga_nat" {
+  nat_gateway_id      = azurerm_nat_gateway.ga_nat.id
+  public_ip_prefix_id = azurerm_public_ip_prefix.ga_nat.id
+}
