5bd1e5b038d3218664ab18128c62b38ee091dd44
Mayur Patel <mayurpatel@ups.com>
2025-04-29 11:22:03 -0400

    bugfix

--- iac/modules/keyvault/kv.tf
+++ iac/modules/keyvault/kv.tf
@@ -1,5 +1,5 @@
 resource "azurerm_key_vault" "keyvault" {
-  name                        = "kv-wwe-${local.environment_sanitized}-${local.region_sanitized}"
+  name                        = "kv2-wwe-${local.environment_sanitized}-${local.region_sanitized}"
   location                    = data.azurerm_resource_group.resource_group.location
   resource_group_name         = data.azurerm_resource_group.resource_group.name
   enabled_for_disk_encryption = true
