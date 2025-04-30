a68ecd6b45858aab284f78c40c7c5b08ff474814
Apkan Nkanta Goshen Mfon (BSJ5CRR) <BSJ5CRR@aad.ups.com>
2025-04-30 11:35:43 -0400

    all web_app deployments for GA

--- iac/wwe-ga/azurerm_windows_web_app.tf
+++ iac/wwe-ga/azurerm_windows_web_app.tf
@@ -34,72 +34,72 @@ module "windows_web_app_globalcheckout" {
 }
 
 
-# module "windows_web_app_carthandoff" {
-#  source = "../modules/app-services"
-#   environment         = local.environment_sanitized
-#   organization_suffix = "wwe"
-#   app_name            = "carthandoff"
-#   app_type            = local.app_type
-#   app_sku             = var.app_sku
-#   region              = local.region_sanitized
-#   asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
-# }
+module "windows_web_app_carthandoff" {
+ source = "../modules/app-services"
+  environment         = local.environment_sanitized
+  organization_suffix = "wwe"
+  app_name            = "carthandoff"
+  app_type            = local.app_type
+  app_sku             = var.app_sku
+  region              = local.region_sanitized
+  asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
+}
 
-# module "windows_web_app_classifying" {
-#  source = "../modules/app-services"
-#   environment         = local.environment_sanitized
-#   organization_suffix = "wwe"
-#   app_name            = "classifying"
-#   app_type            = local.app_type
-#   app_sku             = var.app_sku
-#   region              = local.region_sanitized
-#   asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
-# }
+module "windows_web_app_classifying" {
+ source = "../modules/app-services"
+  environment         = local.environment_sanitized
+  organization_suffix = "wwe"
+  app_name            = "classifying"
+  app_type            = local.app_type
+  app_sku             = var.app_sku
+  region              = local.region_sanitized
+  asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
+}
 
-# module "windows_web_app_webjobs" {
-#  source = "../modules/app-services"
-#   environment         = local.environment_sanitized
-#   organization_suffix = "wwe"
-#   app_name            = "webjobs"
-#   app_type            = local.app_type
-#   app_sku             = var.app_sku
-#   region              = local.region_sanitized
-#   asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
-# }
+module "windows_web_app_webjobs" {
+ source = "../modules/app-services"
+  environment         = local.environment_sanitized
+  organization_suffix = "wwe"
+  app_name            = "webjobs"
+  app_type            = local.app_type
+  app_sku             = var.app_sku
+  region              = local.region_sanitized
+  asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
+}
 
-# module "windows_web_app_hangfire" {
-#  source = "../modules/app-services"
-#   environment         = local.environment_sanitized
-#   organization_suffix = "wwe"
-#   app_name            = "hangfire"
-#   app_type            = local.app_type
-#   app_sku             = var.app_sku
-#   region              = local.region_sanitized
-#   asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
-# }
+module "windows_web_app_hangfire" {
+ source = "../modules/app-services"
+  environment         = local.environment_sanitized
+  organization_suffix = "wwe"
+  app_name            = "hangfire"
+  app_type            = local.app_type
+  app_sku             = var.app_sku
+  region              = local.region_sanitized
+  asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
+}
 
 
-# module "windows_web_app_shopifyintegrationws" {
-#  source = "../modules/app-services"
-#   environment         = local.environment_sanitized
-#   organization_suffix = "wwe"
-#   app_name            = "shopifyintegrationws"
-#   app_type            = local.app_type
-#   app_sku             = var.app_sku
-#   region              = local.region_sanitized
-#   asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
-# }
+module "windows_web_app_shopifyintegrationws" {
+ source = "../modules/app-services"
+  environment         = local.environment_sanitized
+  organization_suffix = "wwe"
+  app_name            = "shopifyintegrationws"
+  app_type            = local.app_type
+  app_sku             = var.app_sku
+  region              = local.region_sanitized
+  asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
+}
 
-# module "windows_web_app_tracking" {
-#  source = "../modules/app-services"
-#   environment         = local.environment_sanitized
-#   organization_suffix = "wwe"
-#   app_name            = "tracking"
-#   app_type            = local.app_type
-#   app_sku             = var.app_sku
-#   region              = local.region_sanitized
-#   asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
-# }
+module "windows_web_app_tracking" {
+ source = "../modules/app-services"
+  environment         = local.environment_sanitized
+  organization_suffix = "wwe"
+  app_name            = "tracking"
+  app_type            = local.app_type
+  app_sku             = var.app_sku
+  region              = local.region_sanitized
+  asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
+}
 
 
 
