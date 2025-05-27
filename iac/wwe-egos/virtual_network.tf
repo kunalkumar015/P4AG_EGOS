2994ea7ed372b60a0cfca79ff0a4ee63ee834528
Apkan Nkanta Goshen Mfon (BSJ5CRR) <BSJ5CRR@aad.ups.com>
2025-05-27 09:12:39 -0400

    moving resources to shared folder

--- iac/wwe-egos/virtual_network.tf
+++ iac/wwe-egos/virtual_network.tf
@@ -29,21 +29,21 @@ resource "azurerm_subnet" "consolidated" {
   }
 }
 
-# resource "azurerm_subnet" "private_endpoint" {
-#   name                 = "snet-privateendpoints"
-#   resource_group_name  = "rg-wwe-${local.environment_sanitized}"
-#   virtual_network_name = azurerm_virtual_network.wwe_egos.name
-#   address_prefixes     = [var.privateendpoints_address_space ]
-#   service_endpoints    = ["Microsoft.Storage","Microsoft.KeyVault","Microsoft.Web","Microsoft.Sql"]
-#   private_endpoint_network_policies = "Disabled"
+resource "azurerm_subnet" "private_endpoint" {
+  name                 = "snet-privateendpoints"
+  resource_group_name  = "rg-wwe-${local.environment_sanitized}"
+  virtual_network_name = azurerm_virtual_network.wwe_egos.name
+  address_prefixes     = [var.consolidated_subnet_address]
+  service_endpoints    = ["Microsoft.Storage","Microsoft.KeyVault","Microsoft.Web","Microsoft.Sql"]
+  private_endpoint_network_policies = "Disabled"
   
-#   lifecycle {
-#     ignore_changes = [
-#      delegation
-#     ]
-#   }
+  lifecycle {
+    ignore_changes = [
+     delegation
+    ]
+  }
 
-# }
+}
 
 resource "azurerm_subnet" "nuget" {
   name                 = "snet-nuget"
