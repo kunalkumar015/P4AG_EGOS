d730760b27d5384265427ba6d0c3cd026e36197a
Apkan Nkanta Goshen Mfon (BSJ5CRR) <BSJ5CRR@aad.ups.com>
2025-05-30 12:17:06 -0400

    resolving sql error/adding webapps

--- iac/wwe-egos/azurerm_windows_web_app.tf
+++ iac/wwe-egos/azurerm_windows_web_app.tf
@@ -1,118 +1,118 @@
-# module "windows_web_app_dashboard" {
-#  source = "../modules/app-services"
-#   environment         = local.environment_sanitized
-#   organization_suffix = "wwe"
-#   app_name            = "dashboard"
-#   app_type            = local.app_type
-#   app_sku             = var.app_sku
-#   region              = local.region_sanitized
-#   asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
-#   subnet_id = azurerm_subnet.webapps.id
+module "windows_web_app_dashboard" {
+ source = "../modules/app-services"
+  environment         = local.environment_sanitized
+  organization_suffix = "wwe"
+  app_name            = "dashboard"
+  app_type            = local.app_type
+  app_sku             = var.app_sku
+  region              = local.region_sanitized
+  asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
+  subnet_id = azurerm_subnet.webapps.id
 
-#   depends_on = [azurerm_subnet.webapps]
-# }
+  depends_on = [azurerm_subnet.webapps]
+}
 
-# module "windows_web_app_webservices" {
-#  source = "../modules/app-services"
-#   environment         = local.environment_sanitized
-#   organization_suffix = "wwe"
-#   app_name            = "webservices"
-#   app_type            = local.app_type
-#   app_sku             = var.app_sku
-#   region              = local.region_sanitized
-#   asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
-#   subnet_id = azurerm_subnet.webapps.id
+module "windows_web_app_webservices" {
+ source = "../modules/app-services"
+  environment         = local.environment_sanitized
+  organization_suffix = "wwe"
+  app_name            = "webservices"
+  app_type            = local.app_type
+  app_sku             = var.app_sku
+  region              = local.region_sanitized
+  asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
+  subnet_id = azurerm_subnet.webapps.id
 
-#   depends_on = [azurerm_subnet.webapps]
+  depends_on = [azurerm_subnet.webapps]
 
-# }
+}
 
-# module "windows_web_app_tradedirect" {
-#  source = "../modules/app-services"
-#   environment         = local.environment_sanitized
-#   organization_suffix = "wwe"
-#   app_name            = "tradedirect"
-#   app_type            = local.app_type
-#   app_sku             = var.app_sku
-#   region              = local.region_sanitized
-#   asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
-#   subnet_id = azurerm_subnet.webapps.id
+module "windows_web_app_tradedirect" {
+ source = "../modules/app-services"
+  environment         = local.environment_sanitized
+  organization_suffix = "wwe"
+  app_name            = "tradedirect"
+  app_type            = local.app_type
+  app_sku             = var.app_sku
+  region              = local.region_sanitized
+  asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
+  subnet_id = azurerm_subnet.webapps.id
 
-#   depends_on = [azurerm_subnet.webapps]
+  depends_on = [azurerm_subnet.webapps]
 
-# }
+}
 
-# module "windows_web_app_opstrackingwebapi" {
-#  source = "../modules/app-services"
-#   environment         = local.environment_sanitized
-#   organization_suffix = "wwe"
-#   app_name            = "opstrackingwebapi"
-#   app_type            = local.app_type
-#   app_sku             = var.app_sku
-#   region              = local.region_sanitized
-#   asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
-#   subnet_id = azurerm_subnet.webapps.id
+module "windows_web_app_opstrackingwebapi" {
+ source = "../modules/app-services"
+  environment         = local.environment_sanitized
+  organization_suffix = "wwe"
+  app_name            = "opstrackingwebapi"
+  app_type            = local.app_type
+  app_sku             = var.app_sku
+  region              = local.region_sanitized
+  asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
+  subnet_id = azurerm_subnet.webapps.id
 
-#   depends_on = [azurerm_subnet.webapps]
+  depends_on = [azurerm_subnet.webapps]
 
-# }
+}
 
-# module "windows_web_app_dao" {
-#  source = "../modules/app-services"
-#   environment         = local.environment_sanitized
-#   organization_suffix = "wwe"
-#   app_name            = "dao"
-#   app_type            = local.app_type
-#   app_sku             = var.app_sku
-#   region              = local.region_sanitized
-#   asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
-#   subnet_id = azurerm_subnet.webapps.id
+module "windows_web_app_dao" {
+ source = "../modules/app-services"
+  environment         = local.environment_sanitized
+  organization_suffix = "wwe"
+  app_name            = "dao"
+  app_type            = local.app_type
+  app_sku             = var.app_sku
+  region              = local.region_sanitized
+  asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
+  subnet_id = azurerm_subnet.webapps.id
 
-#   depends_on = [azurerm_subnet.webapps]
+  depends_on = [azurerm_subnet.webapps]
 
-# }
+}
 
-# module "windows_web_app_egosapi" {
-#  source = "../modules/app-services"
-#   environment         = local.environment_sanitized
-#   organization_suffix = "wwe"
-#   app_name            = "egosapi"
-#   app_type            = local.app_type
-#   app_sku             = var.app_sku
-#   region              = local.region_sanitized
-#   asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
-#   subnet_id = azurerm_subnet.webapps.id
+module "windows_web_app_egosapi" {
+ source = "../modules/app-services"
+  environment         = local.environment_sanitized
+  organization_suffix = "wwe"
+  app_name            = "egosapi"
+  app_type            = local.app_type
+  app_sku             = var.app_sku
+  region              = local.region_sanitized
+  asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
+  subnet_id = azurerm_subnet.webapps.id
 
-#   depends_on = [azurerm_subnet.webapps]
+  depends_on = [azurerm_subnet.webapps]
 
-# }
+}
 
-# module "windows_web_app_hangfire" {
-#  source = "../modules/app-services"
-#   environment         = local.environment_sanitized
-#   organization_suffix = "wwe"
-#   app_name            = "hangfire"
-#   app_type            = local.app_type
-#   app_sku             = var.app_sku
-#   region              = local.region_sanitized
-#   asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
-#   subnet_id = azurerm_subnet.webapps.id
+module "windows_web_app_hangfire" {
+ source = "../modules/app-services"
+  environment         = local.environment_sanitized
+  organization_suffix = "wwe"
+  app_name            = "hangfire"
+  app_type            = local.app_type
+  app_sku             = var.app_sku
+  region              = local.region_sanitized
+  asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
+  subnet_id = azurerm_subnet.webapps.id
 
-#   depends_on = [azurerm_subnet.webapps]
+  depends_on = [azurerm_subnet.webapps]
 
-# }
+}
 
-# module "windows_web_app_nuget" {
-#  source = "../modules/app-services"
-#   environment         = local.environment_sanitized
-#   organization_suffix = "wwe"
-#   app_name            = "nuget"
-#   app_type            = local.app_type
-#   app_sku             = var.app_sku
-#   region              = local.region_sanitized
-#   asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
-#   subnet_id = azurerm_subnet.webapps.id
+module "windows_web_app_nuget" {
+ source = "../modules/app-services"
+  environment         = local.environment_sanitized
+  organization_suffix = "wwe"
+  app_name            = "nuget"
+  app_type            = local.app_type
+  app_sku             = var.app_sku
+  region              = local.region_sanitized
+  asp_zone_balancing_enabled = var.asp_zone_balancing_enabled
+  subnet_id = azurerm_subnet.webapps.id
 
-#   depends_on = [azurerm_subnet.webapps]
+  depends_on = [azurerm_subnet.webapps]
 
-# }
+}
