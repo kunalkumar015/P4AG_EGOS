faa8399120d26f15cac04b4ac49b175a4ac1be64
Apkan Nkanta Goshen Mfon (BSJ5CRR) <BSJ5CRR@aad.ups.com>
2025-07-16 14:41:59 -0400

    changing allocation method for agw pip

--- iac/modules/agw/agw.tf
+++ iac/modules/agw/agw.tf
@@ -3,7 +3,7 @@ resource "azurerm_public_ip" "agw_pip" {
   resource_group_name = var.resource_group_name
   location            = var.location
 
-  allocation_method   = "Dynamic"              # or "Dynamic"
+  allocation_method   = "Static"              # or "Dynamic"
   sku                 = "Standard"            # "Basic" is deprecated for many services
 
   zones               = ["1", "2", "3"]        # Optional: for zone-redundant IP
