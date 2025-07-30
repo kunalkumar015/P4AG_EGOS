1c2a279e2d15022ab508ecb1ce2305013ddcdf0d
Apkan Nkanta Goshen Mfon (BSJ5CRR) <BSJ5CRR@aad.ups.com>
2025-07-30 08:52:04 -0400

    commenting out error configs

--- iac/wwe-egos/keyvault.tf
+++ iac/wwe-egos/keyvault.tf
@@ -1,8 +1,8 @@
-module "keyvault" {
-  source                   = "../modules/keyvault"
-  environment              = local.environment_sanitized
-  organization_suffix      = "wwe"
-  app_type                 = local.app_type
-  region                   = local.region_sanitized
-  tenant_id                = var.tenant_id
-}
+# module "keyvault" {
+#   source                   = "../modules/keyvault"
+#   environment              = local.environment_sanitized
+#   organization_suffix      = "wwe"
+#   app_type                 = local.app_type
+#   region                   = local.region_sanitized
+#   tenant_id                = var.tenant_id
+# }
