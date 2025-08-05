4a99969e8ead41743962563f3a5d8287ee3f726c
Apkan Nkanta Goshen Mfon (BSJ5CRR) <BSJ5CRR@aad.ups.com>
2025-08-05 10:12:37 -0400

    add webapps ip

--- iac/modules/app-services/main.tf
+++ iac/modules/app-services/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_subnet" "webapps" {
   virtual_network_name = "vnet-wwe-${local.app_type}-${local.environment_sanitized}-${local.region_sanitized}"
   resource_group_name  = "rg-wwe-${local.environment_sanitized}"
 
-  address_prefixes = ["172.22.34.128/28"]
+  address_prefixes = ["10.235.89.128/28"]
 }
 
 resource "azurerm_nat_gateway" "nat_gw" {
