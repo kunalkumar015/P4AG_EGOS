e1704d4e2e8f9927d3f82923115602a2668bae52
Apkan Nkanta Goshen Mfon (BSJ5CRR) <BSJ5CRR@aad.ups.com>
2025-08-08 11:16:14 -0400

    adding cosmos db resource

--- iac/modules/cosmosdb/main.tf
+++ iac/modules/cosmosdb/main.tf
@@ -1,26 +1,26 @@
-# locals {
-#   environment_sanitized       = lower(var.environment)
-#   org_suffix_sanitized        = lower(var.organization_suffix)
-#   region_sanitized            = lower(var.region)
-#   app_type                    = lower(var.app_type)
-#   }
+locals {
+  environment_sanitized       = lower(var.environment)
+  org_suffix_sanitized        = lower(var.organization_suffix)
+  region_sanitized            = lower(var.region)
+  app_type                    = lower(var.app_type)
+  }
 
-# data "azurerm_resource_group" "resource_group" {
-#   name = "rg-wwe-${local.environment_sanitized}"
-# }
+data "azurerm_resource_group" "resource_group" {
+  name = "rg-wwe-${local.environment_sanitized}"
+}
 
-# data "azurerm_key_vault" "kv" {
-#   name                = "kv2-wwe-${local.environment_sanitized}-${local.region_sanitized}"
-#   resource_group_name = data.azurerm_resource_group.resource_group.name
+data "azurerm_key_vault" "kv" {
+  name                = "kv2-wwe-${local.environment_sanitized}-${local.region_sanitized}-v3"
+  resource_group_name = data.azurerm_resource_group.resource_group.name
   
-# }
+}
 
-# # data "azurerm_key_vault_secret" "sql_admin_password" {
-# #   name         = "sql-password"
-# #   key_vault_id = data.azurerm_key_vault.kv.id
-# # }
-
-# data "azurerm_key_vault_secret" "cosmosdb_admin_password" {
-#   name         = "cosmosdb-password"
+# data "azurerm_key_vault_secret" "sql_admin_password" {
+#   name         = "sql-password"
 #   key_vault_id = data.azurerm_key_vault.kv.id
 # }
+
+data "azurerm_key_vault_secret" "cosmosdb_admin_password" {
+  name         = "cosmosdb-password"
+  key_vault_id = data.azurerm_key_vault.kv.id
+}
