e7a5ca4eeb592424a9ced6f8421b1e5678f23222
Apkan Nkanta Goshen Mfon (BSJ5CRR) <BSJ5CRR@aad.ups.com>
2025-08-08 14:00:30 -0400

    deploying sql

--- iac/modules/sql/main.tf
+++ iac/modules/sql/main.tf
@@ -12,26 +12,26 @@ data "azurerm_resource_group" "resource_group" {
 data "azurerm_client_config" "current" {}
 
 data "azurerm_key_vault" "kv" {
-  name                        = "kv2-wwe-${local.environment_sanitized}-${local.region_sanitized}-v2"
-  location                    = data.azurerm_resource_group.resource_group.location
+  name                        = "kv2-wwe-${local.environment_sanitized}-${local.region_sanitized}-v3"
   resource_group_name         = data.azurerm_resource_group.resource_group.name
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  sku_name                    = "standard"
+  # tenant_id                   = data.azurerm_client_config.current.tenant_id
+  # sku_name                    = "standard"
+  # location                    = data.azurerm_resource_group.resource_group.location
 
-  purge_protection_enabled    = true
+  # purge_protection_enabled    = true
 
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
+  # access_policy {
+  #   tenant_id = data.azurerm_client_config.current.tenant_id
+  #   object_id = data.azurerm_client_config.current.object_id
 
-    key_permissions         = ["Get", "List"]
-    secret_permissions      = ["Get", "List", "Set"]
-    certificate_permissions = ["Get", "List"]
-  }
+  #   key_permissions         = ["Get", "List"]
+  #   secret_permissions      = ["Get", "List", "Set"]
+  #   certificate_permissions = ["Get", "List"]
+  # }
 
-  tags = {
-    environment = var.environment
-  }
+  # tags = {
+  #   environment = var.environment
+  # }
 }
 
 
@@ -50,3 +50,6 @@ data "azurerm_key_vault" "kv" {
 #   name         = "cosmosdb-password"
 #   key_vault_id = data.azurerm_key_vault.kv.id
 # }
+
+
+
--- iac/wwe-egos/sql_database.tf
+++ iac/wwe-egos/sql_database.tf
@@ -1,57 +1,57 @@
 
-# module "sql" {
-#   source = "../modules/sql"
+module "sql" {
+  source = "../modules/sql"
 
-#   environment                 = local.environment_sanitized
-#   organization_suffix         = "wwe"
-#   region                      = local.region_sanitized
-#   app_type                    = local.app_type
-#   sku_name                    = "S0"
-#   zone_redundant              = false
+  environment                 = local.environment_sanitized
+  organization_suffix         = "wwe"
+  region                      = local.region_sanitized
+  app_type                    = local.app_type
+  sku_name                    = "S0"
+  zone_redundant              = false
 
-#   # Required network-related variables
-#   private_endpoint_subnet_id  = azurerm_subnet.private_endpoint.id
-#   subnet_id                   = azurerm_subnet.webapps.id
-#   sql_admin_password          = var.sql_admin_password
-#   # DNS zone variables
-#   sql_private_dns_zone_id         = azurerm_private_dns_zone.sql.id        # for Azure SQL
+  # Required network-related variables
+  private_endpoint_subnet_id  = azurerm_subnet.shared.id
+  subnet_id                   = azurerm_subnet.shared.id
+  sql_admin_password          = var.sql_admin_password
+  # DNS zone variables
+  sql_private_dns_zone_id         = azurerm_private_dns_zone.sql.id        # for Azure SQL
   
  
+}
+
+
+
+
+# module "sql_database" {
+#   source                = "../modules/sql"
+#   sku_name              = "S0"
+#   zone_redundant        = false
+#   environment           = local.environment_sanitized
+#   organization_suffix   = "wwe"
+#   app_type              = local.app_type
+#   region                = local.region_sanitized
+
+  
+#   private_endpoint_subnet_id = var.subnet_id
+#   private_dns_zone_id        = var.private_dns_zone_id
+#   private_dns_zone_id        = var.cosmosdb_private_dns_zone_id
+#   subnet_id                    = var.subnet_id
+#   cosmosdb_private_dns_zone_id = var.cosmosdb_private_dns_zone_id
 # }
 
 
 
+# module "cosmos_database" {
+#   source              = "../modules/databases"
+#   sku_name            = "Standard"
+#   zone_redundant      = false
+#   environment         = local.environment_sanitized
+#   organization_suffix = "wwe"
+#   app_type            = local.app_type
+#   region              = local.region_sanitized
 
-# # module "sql_database" {
-# #   source                = "../modules/sql"
-# #   sku_name              = "S0"
-# #   zone_redundant        = false
-# #   environment           = local.environment_sanitized
-# #   organization_suffix   = "wwe"
-# #   app_type              = local.app_type
-# #   region                = local.region_sanitized
-
-  
-# #   private_endpoint_subnet_id = var.subnet_id
-# #   private_dns_zone_id        = var.private_dns_zone_id
-# #   private_dns_zone_id        = var.cosmosdb_private_dns_zone_id
-# #   subnet_id                    = var.subnet_id
-# #   cosmosdb_private_dns_zone_id = var.cosmosdb_private_dns_zone_id
-# # }
-
-
-
-# # module "cosmos_database" {
-# #   source              = "../modules/databases"
-# #   sku_name            = "Standard"
-# #   zone_redundant      = false
-# #   environment         = local.environment_sanitized
-# #   organization_suffix = "wwe"
-# #   app_type            = local.app_type
-# #   region              = local.region_sanitized
-
-# #   private_endpoint_subnet_id = var.subnet_id
-# #   private_dns_zone_id        = var.cosmosdb_private_dns_zone_id
-# # }
+#   private_endpoint_subnet_id = var.subnet_id
+#   private_dns_zone_id        = var.cosmosdb_private_dns_zone_id
+# }
 
 
