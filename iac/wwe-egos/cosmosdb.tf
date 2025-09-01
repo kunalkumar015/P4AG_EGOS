e2aa2e19386125ae6180dabb887d75027baa3543
Apkan Nkanta Goshen Mfon (BSJ5CRR) <BSJ5CRR@aad.ups.com>
2025-09-01 11:43:24 -0400

    delete cosmos db old(3.6)

--- iac/wwe-egos/cosmosdb.tf
+++ iac/wwe-egos/cosmosdb.tf
@@ -1,15 +1,15 @@
-module "cosmosdb" {
-  source              = "../modules/cosmosdb"
-  sku_name            = "Standard"
-  zone_redundant      = false
-  environment         = local.environment_sanitized
-  organization_suffix = "wwe"
-  app_type            = local.app_type
-  region              = local.region_sanitized
+# module "cosmosdb" {
+#   source              = "../modules/cosmosdb"
+#   sku_name            = "Standard"
+#   zone_redundant      = false
+#   environment         = local.environment_sanitized
+#   organization_suffix = "wwe"
+#   app_type            = local.app_type
+#   region              = local.region_sanitized
 
-  private_endpoint_subnet_id = var.subnet_id
-  subnet_id                   = azurerm_subnet.shared.id
-  cosmosdb_private_dns_zone_id = azurerm_private_dns_zone.cosmosdb.id  # for Cosmos DB
-  //32 length password for cosmos.
-}
+#   private_endpoint_subnet_id = var.subnet_id
+#   subnet_id                   = azurerm_subnet.shared.id
+#   cosmosdb_private_dns_zone_id = azurerm_private_dns_zone.cosmosdb.id  # for Cosmos DB
+#   //32 length password for cosmos.
+# }
 
